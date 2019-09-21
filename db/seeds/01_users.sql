fc07e47dde20083197bfde17f9e27d4ff9923bda
Colin McEwan <colin-mcewan@hotmail.com>
2019-09-21 19:19:05 -0400

    altered test data

--- db/seeds/01_users.sql
+++ db/seeds/01_users.sql
@@ -1,4 +1,3 @@
--- Users table seeds here (Example)
 INSERT INTO users (name, email, password) VALUES ('Alice', 'alice@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Kira', 'kira@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Colin', 'colin@gmail.com', 'password');
@@ -8,4 +7,4 @@ INSERT INTO users (name, email, password) VALUES ('Dylan', 'dylan@gmail.com', 'p
 INSERT INTO users (name, email, password) VALUES ('Kim kardashian', 'kim123@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Kyle', 'kyle@gmail.com', 'password');
 INSERT INTO users (name, email, password) VALUES ('Katherine', 'katherine321@gmail.com', 'password');
-INSERT INTO users (name, email, password) VALUES ('Michael M', 'kira@gmail.com', 'password');
+INSERT INTO users (name, email, password) VALUES ('Michael M', 'kira123@gmail.com', 'password');
