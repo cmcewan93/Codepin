432fc28204a6f62409ce07ef9cf38bb172f27978
Colin McEwan <colin-mcewan@hotmail.com>
2019-09-24 15:19:26 -0400

    modularized header and created favourites view

---
+++ routes/apiFavourites.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router  = express.Router();
+const favourites = require("../lib/getFavouritesByUser")
+
+module.exports = (db) => {
+  router.get("/", (req, res) => {
+    favourites.getFavouritesByUser(db, 1)
+      .then(resource => {
+        if (!resource) {
+          res.send({error: "error"});
+          return;
+        } else {
+          res.send(resource);
+          
+        }
+      }).catch(err => console.error(null, err.stack));
+  });
+
+  return router;
+};
