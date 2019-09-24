f49297d3f89ffa1785707124dd6c0b3f7bd328d3
karlyabut <karleisenyabut@gmail.com>
2019-09-24 18:45:19 +0000

    Implemented rendering resources dynamically into page

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,59 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// import { render } from "node-sass";
+
+// // // // // $(() => {
+// // // // //   $.ajax({
+// // // // //     method: "GET",
+// // // // //     url: "/api/users"
+// // // // //   }).done((users) => {
+// // // // //     for(user of users) {
+// // // // //       $("<div>").text(user.name).appendTo($("body"));
+// // // // //     }
+// // // // //   });;
+// // // // // });
 
 //GET THE RESOURCES from the local api
+
+const createResourcesElement = function(value) {
+  console.log(value, "asdhakjdhas");
+  // <img src="${value.imgUrl}">
+  const $resource = `
+  <div class="column">
+    <img src="${value}" style="width:100%"/>
+  </div>
+  `;
+  return $resource;
+}
+
+const renderResources = function(resources) {
+  for(let resource in resources) {
+    resources[resource].forEach(element => {
+      // console.log(element.title)
+      $(document).ready(()=> {
+        // console.log("Asdkaksdas", element);
+        $('#resource-container').prepend(createResourcesElement(element.imgurl));
+      })
+    });
+
+  }
+}
+
 $.ajax({
   method: "GET",
   url: "/api/resources"
 }).done((resources) => {
-  console.log("aWTFWFWFWF");
-  for(resource in resources) {
-    console.log("asjhdakjshd", resources);
-    resources[resource].forEach(element => {
-      $("<div>").text(element.title).appendTo($("body"));
-      $("<div>").text(element.description).appendTo($("body"));
-      $("<div>").text(element.created_at).appendTo($("body"));
-    });
-  }
+  // console.log(resources, "asjdjahkda");
+  renderResources(resources);
+  // console.log("aWTFWFWFWF");
+  // for(resource in resources) {
+  //   console.log("asjhdakjshd", resources);
+  //   resources[resource].forEach(element => {
+  //     $("<div>").text(element.title).appendTo($("body"));
+  //     $("<div>").text(element.description).appendTo($("body"));
+  //     $("<div>").text(element.created_at).appendTo($("body"));
+  //   });
+  // }
 });;
+
 
 //GET THE SEARCHED RESOURCES
 $.ajax({
@@ -34,9 +64,10 @@ $.ajax({
   for(resource in resources) {
     console.log("asjhdakjshd", resources);
     resources[resource].forEach(element => {
-      $("<div>").text(element.title).appendTo($("body"));
-      $("<div>").text(element.description).appendTo($("body"));
-      $("<div>").text(element.created_at).appendTo($("body"));
+      // $("<div>").text(element.title).appendTo($("#container"));
+      // $("<div>").text(element.description).appendTo($("#container"));
+      // $("<div>").text(element.created_at).appendTo($("#container"));
+
     });
   }
 });;
