736c35bce592871a64ac0b89671bf8c57a948659
Colin McEwan <colin-mcewan@hotmail.com>
2019-09-24 21:19:00 -0400

    adjusted views

--- public/scripts/favourites.js
+++ public/scripts/favourites.js
@@ -1,7 +1,7 @@
 const createResourcesElement = function(value) {
   // <img src="${value.imgUrl}">
   const $resource = `
-  <div class="column">
+  <div class="column" id="${value.resource_id}">
     <article class="favourite-display">
       <header class="favourite-header">
         <span>${value.title}</span>
@@ -19,9 +19,14 @@ const createResourcesElement = function(value) {
 const renderResources = function(resources) {
   console.log(resources);
   for(let resource in resources) {
-    console.log(resources[resource].imgurl)
+    //console.log(resources[resource].imgurl)
       $(document).ready(()=> {
         $('#favourite-container').prepend(createResourcesElement(resources[resource]));
+        $(".column").on('click', function(e) {
+          e.preventDefault();
+          loadFavouriteModal(resources[resource]);
+          // $("#favourite-modal").slideToggle();
+        });
       })
   }
 }
@@ -36,6 +41,31 @@ const loadFavourites = () => {
   });;
 }
 
+const loadFavouriteModal = (resource) => {
+  $('body').append(`
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  `)
+}
+
 const convertDate = (dateobj) => {
   let date = new Date(parseInt(dateobj));
   let fdate = (date.getMonth() + 1)+'/'+ date.getDate()  +'/'+date.getFullYear()
@@ -43,5 +73,6 @@ const convertDate = (dateobj) => {
 }
 
 loadFavourites();
+
 
 
