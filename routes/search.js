f03ed2399d935f6852b9d59ed03e7714f3c3ec54
karlyabut <karleisenyabut@gmail.com>
2019-09-26 17:56:19 +0000

    Search resource partially done

--- routes/search.js
+++ routes/search.js
@@ -7,6 +7,7 @@ module.exports = () => {
    // const {tag} = req.body;
    // console.log(req.query);
     //console.log('Logging req body    ', req.body )
+    console.log(req.query.tag);
     search.searchByTag(req.query.tag)
       .then(resource => {
         console.log('this is the resource', resource)
@@ -15,12 +16,6 @@ module.exports = () => {
           return;
         } else {
           res.json({ resource });
-          // resource.forEach(element => {
-          //   console.log(element.title);
-          // });
-          // res.render('resources');
-          //console.log("asasd", resource);
-          // res.send("BAITCH");
         }
       }).catch(err => console.error(null, err.stack));
   });
