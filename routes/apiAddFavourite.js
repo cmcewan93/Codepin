8a01b662fd3a6709759f01dafeb484cc3f3ccbd4
Colin McEwan <colin-mcewan@hotmail.com>
2019-09-26 22:19:51 -0400

    fixed toggle function when adding favourite

--- routes/apiAddFavourite.js
+++ routes/apiAddFavourite.js
@@ -14,8 +14,6 @@ module.exports = () => {
    favourite.id = req.body.resource.id;
    favourite.user = req.session.userId;
 
-   //console.log('@@@@@@', user);
-   //console.log('HERES THE ID TO ADD', req.body.resource);
     favourites.addFavourite(favourite)
       .then(resource => {
         if (!resource) {
--- routes/resources.js
+++ routes/resources.js
@@ -11,10 +11,11 @@ const resources = require('../database');
 module.exports = () => {
   router.get("/", (req, res) => {
     let templateVars = {};
-    console.log("id", req.session.userId);
+    //console.log("id", req.session.userId);
     Promise.all([resources.getUser(req.session.userId)])
       .then((values) => {
         templateVars.user = values[0];
+        //console.log('SADFSDAFSADFASDF', values)[0];
         res.render("resources", templateVars);
       })
       .catch(err => console.error(null, err.stack));
