c51369ab5cf63be4b7ddd6c0dd26d64729202b0e
karlyabut <karleisenyabut@gmail.com>
2019-09-27 02:31:37 +0000

    Added showing likes for each resource

--- public/scripts/search.js
+++ public/scripts/search.js
@@ -1,68 +1,69 @@
+// const createResourcesElement = function(value) {
+//   // console.log(value.id);
+//   const renderedResourcesArray = [];
+//   if(!value.imgurl) {
+//     renderedResourcesArray.push(`
+//     <div id="${ value.id }" class="card text-white text-center p-3" data-toggle="modal" data-target="#exampleModalCenter">
+//       <blockquote class="blockquote mb-0">
+//         <p>${ value.description }</p>
+//         <footer class="blockquote-footer">
+//           <small>
+//             Someone famous in <cite title="Source Title">Source Title</cite>
+//           </small>
+//         </footer>
+//       </blockquote>
+//     </div>
+//     `)
+//   } else if(!value.title && !value.description) {
+//     renderedResourcesArray.push(`
+//     <div id="${ value.id }" class="card" data-toggle="modal" data-target="#exampleModalCenter">
+//       <img class="card-img" src="${ value.imgurl }" alt="Card image">
+//     </div>
+//     `)
+//   } else {
+//     renderedResourcesArray.push(`
+//     <div id="${ value.id }" class="card" data-toggle="modal" data-target="#exampleModalCenter">
+//       <img class="card-img-top" src="${ value.imgurl }" alt="Card image cap">
+//       <div class="card-body">
+//         <h5 class="card-title">${ value.title }</h5>
+//         <p class="card-text">${ value.description }</p>
+//       </div>
+//     </div>
+//     `)
+//   }
+//   return renderedResourcesArray.join('');
+// }
 
-const createResourcesElement = function(value) {
-  // console.log(value.id);
-  const renderedResourcesArray = [];
-  if(!value.imgurl) {
-    renderedResourcesArray.push(`
-    <div id="${ value.id }" class="card text-white text-center p-3" data-toggle="modal" data-target="#exampleModalCenter">
-      <blockquote class="blockquote mb-0">
-        <p>${ value.description }</p>
-        <footer class="blockquote-footer">
-          <small>
-            Someone famous in <cite title="Source Title">Source Title</cite>
-          </small>
-        </footer>
-      </blockquote>
-    </div>
-    `)
-  } else if(!value.title && !value.description) {
-    renderedResourcesArray.push(`
-    <div id="${ value.id }" class="card" data-toggle="modal" data-target="#exampleModalCenter">
-      <img class="card-img" src="${ value.imgurl }" alt="Card image">
-    </div>
-    `)
-  } else {
-    renderedResourcesArray.push(`
-    <div id="${ value.id }" class="card" data-toggle="modal" data-target="#exampleModalCenter">
-      <img class="card-img-top" src="${ value.imgurl }" alt="Card image cap">
-      <div class="card-body">
-        <h5 class="card-title">${ value.title }</h5>
-        <p class="card-text">${ value.description }</p>
-      </div>
-    </div>
-    `)
-  }
-  return renderedResourcesArray.join('');
-}
+// const renderResources = function(resources) {
+//   for(let resource in resources) {
+//     console.log(resources[resource]);
+//     // resources[resource].forEach(element => {
+//     //   $('#searched-resources').prepend(createResourcesElement(element));
+//     //   //Redirect to a specific resource
+//     //   $(`#${ element.id }`).click(function() {
+//     //     // alert($(title).text());
+//     //     // window.location.href = `/resources/${ element.id }`;
+//     //   });
 
-const renderResources = function(resources) {
-  for(let resource in resources) {
-    resources[resource].forEach(element => {
-      $('#searched-resources').prepend(createResourcesElement(element));
-      //Redirect to a specific resource
-      $(`#${ element.id }`).click(function() {
-        // alert($(title).text());
-        // window.location.href = `/resources/${ element.id }`;
-      });
-
-      $(`#${ element.id }`).hover(function() {
-        $(`#${ element.id }`).css("opacity", 0.5);
-      }, function() {
-        $(`#${ element.id }`).css("opacity", 1);
-      });
-    });
-  }
-}
+//     //   $(`#${ element.id }`).hover(function() {
+//     //     $(`#${ element.id }`).css("opacity", 0.5);
+//     //   }, function() {
+//     //     $(`#${ element.id }`).css("opacity", 1);
+//     //   });
+//     // });
+//   }
+// }
 
 
-//GET THE SEARCHED RESOURCES
+// //GET THE SEARCHED RESOURCES
 const searchResource = () => {
   $.ajax({
     method: "GET",
     url: "/api/searchedResources"
   }).done((resources) => {
-    renderResources(resources);
+    // renderResources(resources);
+    console.log("resource", resources);
   });;
 }
 
-searchResource();
+// searchResource();
--- routes/search.js
+++ routes/search.js
@@ -4,16 +4,37 @@ const search = require("../database")
 
 module.exports = () => {
   router.get("/", (req, res) => {
-    let templateVars = {};
-    console.log("id", req.session.userId);
-    console.log("WTF", req.body)
-    Promise.all([search.getUser(req.session.userId)])
-      .then((values) => {
-        templateVars.user = values[0];
-        res.render("searchedResource", templateVars);
-      })
-      .catch(err => console.error(null, err.stack));
+    // res.send('OK');
+    search.searchByTag(req.query.tag)
+      .then(resource => {
+        console.log('this is the resource', resource)
+        if (!resource) {
+          res.send({error: "error"});
+          return;
+        } else {
+          // res.json({ resource });
+        let templateVars = { resources: resource };
+        console.log("id", req.session.userId);
+        console.log("WTF", req.body)
+        Promise.all([search.getUser(req.session.userId)])
+          .then((values) => {
+            templateVars.user = values[0];
+            res.render("searchedResource", templateVars);
+          })
+          .catch(err => console.error(null, err.stack));
+        }
+      }).catch(err => console.error(null, err.stack));
   });
+
+  // router.get("/", (req, res) => {
+  //   let templateVars = {};
+  //   Promise.all([search.getUser(req.session.userId)])
+  //     .then((values) => {
+  //       templateVars.user = values[0];
+  //       res.render("searchedResource", templateVars);
+  //     })
+  //     .catch(err => console.error(null, err.stack));
+  // });
 
   return router;
 };
